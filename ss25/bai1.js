a9b9a80edadadb62b8ac9eb9100c0167f61663c4
lhn0911 <154567669+lhn0911@users.noreply.github.com>
2024-01-16 08:17:02 +0700

    Update bai1.js

--- ss25/bai1.js
+++ ss25/bai1.js
@@ -1,13 +1,19 @@
-let a= +prompt('Nhập a');;
-let tongle = 0;
-if(!isNaN(a) && a > 0) {
-    for (let i = 1; i <= a; i++) {
-        console.log(i);
-        if (i % 2 !== 0) {
-            tongle += i;
+let number = +prompt("Nhập vào số nguyên dương bất kỳ:");
+let sum = 0;
+
+if (!isNaN(number)) {
+    let i = 1;
+
+    while (i <= number) {
+        if ((number % 4 === 0 && i <= number - 2) || (number % 4 === 3 && i <= number - 1) || (number % 4 !== 0 && number % 4 !== 3)) {
+            sum += i;
+            console.log(i);
         }
+
+        i++;
     }
+
+    console.log('Tổng: ' + sum);
 } else {
-    console.log("Vui lòng nhập một số nguyên dương.");
+    console.log("Không hợp lệ");
 }
-console.log('Tổng các số lẻ từ 1 đến ' + a + ' là: ' + tongle);
